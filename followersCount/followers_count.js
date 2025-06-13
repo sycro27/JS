089a02ce751ec5aaacae6356581417e52d2f6bd1
sycro27 <bilalshakeel27@hotmail.com>
2025-06-13 16:56:51 -0400

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -10,10 +10,14 @@ function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
     }
 
-    function checkCountValue() {
+function checkCountValue() {
         if (count === 10) {
           alert("Your Instagram post gained 10 followers! Congratulations!");
         } else if (count === 20) {
           alert("Your Instagram post gained 20 followers! Keep it up!");
         }
       }
+
+function resetcounter(){
+    document.getElementById('countDisplay').innerHTML = 0;
+}
