fb739707647486cc86e1b1e1042df7482fc16b56
sycro27 <bilalshakeel27@hotmail.com>
2025-06-13 18:49:04 -0400

    message

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ let books = [];
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -46,3 +47,8 @@ function editbook(index) {
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+ function deletebook(index) {
+    books.splice(index, 1);
+    showbooks(); 
+}
